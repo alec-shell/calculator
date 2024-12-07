0598ea41d41f1d70b52cf5412269c776006c9775
alec-shell <143364125+alec-shell@users.noreply.github.com>
2024-12-07 13:34:36 -0500

    Added limit for '.' input and 'equals' buttons. Disabled number input after 'equals' input until new sign is selected.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let leadingTerm = '';
 let trailingTerm = '';
 let sign = '';
+let solved = true;
+let isSolvedTerm = false;
 
 let number = document.querySelectorAll("#number");
 let operator = document.querySelectorAll("#operator");
@@ -14,36 +16,57 @@ operator.forEach(op => op.addEventListener("click", e => getOperator(e.target.va
 equals.addEventListener("click", () => returnEquals());
 AC.addEventListener("click", () => allClear());
 
+
 function getNumber(number) {
-    if (!leadingTerm || !(sign)) {
-        leadingTerm += number;
+    if (!isSolvedTerm) {
+        //Disallow more than one '.' per leadingTerm.
+        if (number === '.' && leadingTerm.includes('.')) {
+            number = '';
+        }
+        //After page refresh or allClear()
+        if (!leadingTerm || !sign) {
+            leadingTerm += number;  
+        }
+        //Move initial variable and sign to trailingTerm; start new leadingTerm
+        else if (!trailingTerm && sign) { 
+            solved = false;
+            trailingTerm = leadingTerm;
+            leadingTerm = '';
+            leadingTerm = number;
+        }
+        //TrailingTerm, leadingTerm and sign established. Solved set to false to allow use of 'equals' button. 
+        else {
+            solved = false;
+            leadingTerm += number;
+        }
+        updateDisplay(leadingTerm, trailingTerm, sign);
     }
-    else if (!trailingTerm && sign) {
-        trailingTerm = leadingTerm;
-        leadingTerm = '';
-        leadingTerm = number;
-    }
-    else {
-        leadingTerm += number;
-    }
-    updateDisplay(leadingTerm, trailingTerm, sign);
-};
+}
+
 
 function getOperator(op) {
-    if (!trailingTerm && leadingTerm) {
+    isSolvedTerm = false;
+    //Declare sign operator after establishing initial term. 
+    if (!trailingTerm && leadingTerm && leadingTerm != '-') {
         sign = op;
     }
+    //Keep running total in trailingTerm slot, establish new sign and free leadingTerm for new number entry. 
     else if (leadingTerm && trailingTerm) {
         trailingTerm = updateValue(leadingTerm, trailingTerm, sign);
         sign = op;
         leadingTerm = '';
-        console.log(trailingTerm);
     }
+    //Allow changing sign after running total if leadingTerm not yet established. 
     else if (!leadingTerm && trailingTerm) {
         sign = op;
     }
+    //Allow initial negative signed int after page refresh or allClear().
+    else if (!leadingTerm && !trailingTerm && op == '-') {
+        leadingTerm = op;
+    }
     updateDisplay(leadingTerm, trailingTerm, sign);
-};
+}
+
 
 function updateValue(a, b, op) {
     let value = 0;
@@ -64,19 +87,27 @@ function updateValue(a, b, op) {
                 value = b / a;
                 break;
         }
+        //mitigate floating point artifacts. 
         if (!(value.toFixed(3) % 1)) {
             return Math.round(value);
         }
         return (value.toFixed(3));
     }
-};
+}
+
 
 function returnEquals() {
-    trailingTerm = updateValue(leadingTerm, trailingTerm, sign);
-    leadingTerm = '';
-    sign = '';
-    updateDisplay(leadingTerm, trailingTerm, sign, true);
-};
+    //'solved' boolean check prevents 'equals' event if new sign and leadingTerm haven't been selected.
+    if (!solved) {
+        solved = true;
+        leadingTerm = updateValue(leadingTerm, trailingTerm, sign);
+        trailingTerm = '';
+        sign = '';
+        updateDisplay(leadingTerm, trailingTerm, sign);
+        isSolvedTerm = true;
+    }
+}
+
 
 function allClear() {
     leadingTerm = '';
@@ -85,18 +116,19 @@ function allClear() {
     result = '';
     lowerDisp.innerText = '';
     upperDisp.innerText = '';
-};
+    solved = true;  //Disallows 'equals' to trigger.  Prevents undefined error.  
+    isSolvedTerm = false;
+}
 
-function updateDisplay(leadingTerm, trailingTerm, sign, enter = false) {
+
+function updateDisplay(leadingTerm, trailingTerm, sign) {  
+    //After page refresh or allClear().  Initial variable entry. 
     if (!trailingTerm) {
         lowerDisp.innerText = `${leadingTerm} ${sign}`;
     }
-    else if (trailingTerm && !enter) {
+    //Everything else...  Displays running total and sign on top, working variable on bottom. 
+    else if (trailingTerm) {
         upperDisp.innerText = `${trailingTerm} ${sign}`;
         lowerDisp.innerText = leadingTerm;
-    }
-    if (enter) {
-        upperDisp.innerText = '';
-        lowerDisp.innerText = trailingTerm;
     }
 }
